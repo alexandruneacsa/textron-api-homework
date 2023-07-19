5b402d51df3b939fa30c85d67368b9aeaf8610d6
Alexandru Neacsa <Alexandru-Valentin.Neacsa@Endava.com>
2023-07-19 16:32:48 +0300

    Add project files.

---
+++ Controllers/InfosController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TextronApp.Data;
+using TextronApp.Models;
+
+namespace TextronApp.Controllers
+{
+    public class InfosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InfosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Infos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Infos.ToListAsync());
+        }
+
+        // GET: Infos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var info = await _context.Infos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (info == null)
+            {
+                return NotFound();
+            }
+
+            return View(info);
+        }
+
+        // GET: Infos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Infos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,GPSlocation,Acceleration,Speed,Date")] Info info)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(info);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(info);
+        }
+
+        // GET: Infos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var info = await _context.Infos.FindAsync(id);
+            if (info == null)
+            {
+                return NotFound();
+            }
+            return View(info);
+        }
+
+        // POST: Infos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GPSlocation,Acceleration,Speed,Date")] Info info)
+        {
+            if (id != info.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(info);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InfoExists(info.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(info);
+        }
+
+        // GET: Infos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var info = await _context.Infos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (info == null)
+            {
+                return NotFound();
+            }
+
+            return View(info);
+        }
+
+        // POST: Infos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var info = await _context.Infos.FindAsync(id);
+            _context.Infos.Remove(info);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InfoExists(int id)
+        {
+            return _context.Infos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextronApp.Models
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        public string GPSlocation { get; set; }
+        public int Acceleration { get; set; }
+        public int Speed { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
